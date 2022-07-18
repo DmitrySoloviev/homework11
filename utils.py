2d664e32a289e0f55a5d5d7de66013ddf1a99668
Дмитрий <DmitrySoloviev>
2022-07-18 15:30:32 +0300

    Changed utils.py

--- utils.py
+++ utils.py
@@ -1,9 +1,9 @@
 import json
 
 
-def load_candidates():
+def load_candidates(path):
     """Загружает список кандидатов из файла"""
-    with open('candidates.json', 'r', encoding='utf-8') as file:
+    with open(path, 'r', encoding='utf-8') as file:
         list_candidates = json.load(file)
         file.close()
         return list_candidates
@@ -13,43 +13,35 @@ def load_candidates():
 def get_by_id(id_):
     """Возвращает кандидатов по номеру"""
     global picture_link
-    list_candidates = load_candidates()
-    message = ""
+    list_candidates = load_candidates('candidates.json')
+    single_candidate = {}
     for candidate in list_candidates:
         if candidate["id"] == id_:
-            picture_link = candidate['picture']
-            message = message + candidate['name'] + "\n"
-            message = message + candidate['position'] + "\n"
-            message = message + candidate['skills'] + "\n" + "\n"
+            single_candidate = candidate
         else:
             pass
-    return f"<img src='({picture_link})'>\n{message}"
+    return single_candidate
 
 
 def get_by_skill(skill_name):
     """Возвращает кандидатов по навыкам"""
-    list_candidates = load_candidates()
-    message = ""
+    list_candidates = load_candidates('candidates.json')
+    list_candidate_by_skills = []
     for candidate in list_candidates:
         list_skills = (candidate["skills"]).split(', ')
         if skill_name.lower() in list_skills or skill_name.title() in list_skills:
-            message = message + candidate['name'] + "\n"
-            message = message + candidate['position'] + "\n"
-            message = message + candidate['skills'] + "\n" + "\n"
+            list_candidate_by_skills.append(candidate)
         else:
             pass
-    return message
+    return list_candidate_by_skills
 
 
-def get_by_name(name):
     """Возвращает кандидатов по имени"""
-    list_candidates = load_candidates()
-    message = ""
+    list_candidates = load_candidates('candidates.json')
+    candidate_by_name = {}
     for candidate in list_candidates:
-        if name.lower() == candidate['name'].lower:
-            message = message + candidate['name'] + "\n"
-            message = message + candidate['position'] + "\n"
-            message = message + candidate['skills'] + "\n" + "\n"
+        if name.lower() == candidate['name'].lower():
+            candidate_by_name = candidate
         else:
             pass
-    return message
+    return candidate_by_name
